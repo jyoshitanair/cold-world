cf5be8ba35df2d9c1bbf0a121a1a313fe03acbfc
jyoshitanair <jyoshitanair123@gmail.com>
2026-06-20 02:47:10 -0700

    added tileset and checks for the leaderboard

--- scripts/text_edit.gd
+++ scripts/text_edit.gd
@@ -1,19 +1,33 @@
 extends Node2D
-@onready var text_edit: TextEdit = $TextEdit
+@onready var line_edit: LineEdit = $TextEdit
 @onready var button: Button = $Button
-
-# Called when the node enters the scene tree for the first time.
+@onready var fail: Label = $fail
+var loaded = false
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
+	await SilentWolf.Scores.get_scores(0).sw_get_scores_complete
+	loaded = true 
 func _on_button_pressed() -> void:
-	button.disabled = true
-	Manager.player_name = text_edit.text
-	var sw_result: Dictionary = await SilentWolf.Scores.save_score(Manager.player_name, Manager.score).sw_save_score_complete
-	get_tree().change_scene_to_file("res://scenes/ending.tscn")
+	if loaded:
+		print("clicked")
+		button.disabled = true
+		var check_value =check_name()
+		print(check_value) 
+		if check_value == false:
+			print("true")
+			Manager.player_name = line_edit.text
+			var sw_result: Dictionary = await SilentWolf.Scores.save_score(Manager.player_name, Manager.score).sw_save_score_complete
+			get_tree().change_scene_to_file("res://scenes/ending.tscn")
+		else:
+			button.disabled = false
+			fail.show()
+func check_name()-> bool:
+	print(line_edit.text)
+	if line_edit.text.strip_edges().to_lower() == "":
+		fail.text = "empty."
+		print("empty")
+		return true
+	for score_data in SilentWolf.Scores.scores:
+			if score_data["player_name"].strip_edges().to_lower() ==  line_edit.text.strip_edges().to_lower():
+				fail.text = "used."
+				return true
+	return false

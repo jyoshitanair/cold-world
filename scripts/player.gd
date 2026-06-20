c8a5d8ee40aab8833e49d693c169ea846f3402c4
chelsea-zh <chelseazh2025@gmail.com>
2026-06-20 00:53:16 -0700

    working test with both players

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,32 @@
 class_name Player extends CharacterBody2D
 
 @export var playerLeft:bool
+var playerRight:Player
 
-#manager vars: speed, jump, bool: double jump, unity
+#manager vars: speedMult, jumpMult, bool: double jump, unity
+
+var speedMult:float = 1
+var jumpMult:float = 1
 
 var doubleJump = true
 var jumped = false
 
 var warmth:int
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speed = 300.0 * speedMult
+var jumpVelocity = -400.0 * jumpMult
 
+func _ready() -> void:
+	for child in get_parent().get_children():
+		if child is Player:
+			if child.playerLeft != true:
+				playerRight = child
 
 func _physics_process(delta: float) -> void:
+	# determine warmth through distance
+	if (playerLeft and playerRight != null):
+		var dist = global_position.distance_to(playerRight.global_position)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -22,7 +35,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("player_1_jump" if playerLeft else "player_2_jump") and (is_on_floor() or (doubleJump and !jumped)):
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jumpVelocity
 		if not is_on_floor():
 			jumped = true
 
@@ -30,8 +43,8 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis ("player_1_left" if playerLeft else "player_2_left", "player_1_right" if playerLeft else "player_2_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()

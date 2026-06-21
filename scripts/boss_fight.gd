35c54a2700fc54cb07b991d50da127fb71ab5751
jyoshitanair <jyoshitanair123@gmail.com>
2026-06-21 00:26:15 -0700

    boss fight!

--- scripts/boss_fight.gd
+++ scripts/boss_fight.gd
@@ -3,19 +3,35 @@ extends Node2D
 @onready var player_2: CharacterBody2D = $player2
 @onready var marker1: Marker2D = $CharacterBody2D/Marker2D
 @onready var marker2: Marker2D = $player2/Marker2D
+@onready var label_2: Label = $Label2
+@onready var label: Label = $Label
 var snowball_path = preload("res://scenes/snowball.tscn")
+var total_time = 0.0
+var yeti_health = 100
 func _ready() -> void:
 	pass
 func _physics_process(delta: float) -> void:
+	if yeti_health <= 0:
+		Manager.final_time = total_time
+		get_tree().change_scene_to_file("res://scenes/buffs.tscn")
+	total_time += delta
+	label_2.text = "Time Elapsed: %d"%total_time
+	label.text = "Yeti Health: %d"%yeti_health
 	if Input.is_action_just_pressed("e"):
 		var snowball = snowball_path.instantiate()
 		add_child(snowball)
 		snowball.global_position = marker1.global_position
-	#if Input.is_action_just_pressed("p"):
-	
-
+		snowball.dir = 1
+	if Input.is_action_just_pressed("p"):
+		var snowball = snowball_path.instantiate()
+		add_child(snowball)
+		snowball.global_position = marker2.global_position
+		snowball.dir = -1
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print(area.get_groups())
-	if area.is_in_group("snowball"):
-		area.queue_free()
+	print(area.get_parent().name)
+	if area.get_parent().is_in_group("snowball"):
+		area.get_parent().queue_free()
+		area.hide()
+		yeti_health -= 0.5
--- scripts/snowball.gd
+++ scripts/snowball.gd
@@ -1,6 +1,10 @@
 extends Node2D
-
-
+var dir = 1
+func _ready() -> void:
+	add_to_group("snowball")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position.x += delta*800
+	if dir == 1:
+		global_position.x += delta*800
+	else:
+		global_position.x -= delta*800

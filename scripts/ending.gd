c67dfd38bfb1d6869b1d2fd2a800ab86a277fc1d
jyoshitanair <jyoshitanair123@gmail.com>
2026-06-21 13:33:55 -0700

    skfjs;lk

--- scripts/ending.gd
+++ scripts/ending.gd
@@ -4,7 +4,7 @@ extends Node2D
 var score = 0 
 @onready var button: Button = $Button
 func _on_button_pressed() -> void:
-	#button.disabled = true
-	#score = randi_range(0,9)
-	#Manager.score = score
+	Manager.score = Manager.level_one_calc + Manager.level_two_calc + Manager.level_three_calc
+	var player_name = Manager.player_name
+	SilentWolf.Scores.save_score(Manager.player_name, Manager.score, "main")
 	get_tree().change_scene_to_file("res://scenes/leaderboard.tscn")

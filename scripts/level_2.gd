a35085f6849429ddea112f06fc85ecd3bc71443c
jyoshitanair <jyoshitanair123@gmail.com>
2026-06-21 13:17:35 -0700

    skdj;lk

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -1,11 +1,19 @@
-extends Node2D
-
+extends Level
+@onready var block: Label = $tutorial/block
+@onready var pressure: Label = $tutorial/pressure
+@onready var tutorial: Control = $tutorial
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	super()
+	tutorial.visible = true
+	pressure.hide()
+	pressure.visible = false
+	block.visible = false
+	block.hide()
+func _on_tut_2_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		pressure.visible = true
+func _on_tut_3_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		block.visible = true
